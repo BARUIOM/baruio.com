62e7b4da5b7faa52abcde443737ec8dd133bbdae
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-04-27 23:23:29 -0300

    Added TailwindCSS

---
+++ postcss.config.js
@@ -0,0 +1,11 @@
+import tailwind from "tailwindcss";
+import autoprefixer from "autoprefixer";
+
+import config from "./tailwind.config.js";
+
+export default {
+    plugins: [
+        tailwind(config),
+        autoprefixer()
+    ],
+}
