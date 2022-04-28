3d8ce5ffb017cd271cdb8efb2026e0fb33dbf27a
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-04-27 23:54:31 -0300

    Main page container

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<main>
+    <div class="w-screen h-screen flex flex-col">
+        <div class="h-16 border-b border-white"></div>
+        <div class="h-full"></div>
+        <div class="h-24 border-t border-white"></div>
+    </div>
+</main>
+
+<style lang="scss">
+</style>
