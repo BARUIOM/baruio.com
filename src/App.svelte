d1bad5ead3ec5cd2ba50dd4e49dba5d4dcc1cb42
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-04-28 16:24:37 -0300

    Added svelte-spa-router

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,15 @@
 <script lang="ts">
+    import Router from "svelte-spa-router";
+    import routes from "./routes";
 </script>
 
 <main>
     <div class="w-screen h-screen flex flex-col">
-        <div class="h-16 border-b border-white"></div>
-        <div class="h-full"></div>
-        <div class="h-24 border-t border-white"></div>
+        <div class="h-16 border-b border-white" />
+        <div class="h-full">
+            <Router {routes} />
+        </div>
+        <div class="h-24 border-t border-white" />
     </div>
 </main>
 
---
+++ src/routes/index.ts
@@ -0,0 +1,6 @@
+import type { RouteDefinition } from "svelte-spa-router";
+
+const routes: RouteDefinition = {
+};
+
+export default routes;
