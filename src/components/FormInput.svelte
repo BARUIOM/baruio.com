6a5a63b5bfc77e180f66841d6a34ec6cc5e2380a
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-04-30 00:10:40 -0300

    Custom input component implementation

---
+++ src/components/FormInput.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let label: string;
+    export let rounded: boolean = false;
+
+    export let value: string = "";
+    export let type: "text" | "password" = "text";
+
+    const onInput = (event: Event) => {
+        value = (<HTMLInputElement>event.target).value;
+    };
+</script>
+
+<div>
+    <div class="text-sm">{label}</div>
+    <input class:pill={rounded} {type} {value} on:input={onInput} />
+</div>
+
+<style lang="scss">
+    input {
+        @apply w-full h-8 px-2 mt-1 rounded bg-neutral-800 outline-none;
+    }
+
+    input.pill {
+        @apply rounded-full;
+    }
+</style>
