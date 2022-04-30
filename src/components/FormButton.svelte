deec77b1250636966bc5f3994d61b8e3d4365138
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-04-30 02:02:21 -0300

    Custom button component implementation

---
+++ src/components/FormButton.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+    import chroma from "chroma-js";
+
+    export let label: string;
+    export let color: string;
+
+    export let click = (event?: Event) => {};
+
+    const backgroundColor = chroma(color);
+    const hoverColor = chroma(color).luminance(0.2, "hsl");
+</script>
+
+<div>
+    <button
+        on:click={click}
+        style="
+            --background-color: {backgroundColor};
+            --hover-color: {hoverColor};
+        "
+    >
+        {label}
+    </button>
+</div>
+
+<style lang="scss">
+    button {
+        @apply w-full h-8 rounded;
+        background-color: var(--background-color);
+    }
+
+    button:hover {
+        background-color: var(--hover-color);
+    }
+</style>
