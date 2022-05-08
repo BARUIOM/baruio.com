ab6d93bc148791604936a729a8b99b38919892ed
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-05-07 23:06:43 -0300

    Basic home layout

--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -21,14 +21,28 @@
     <Loading />
 {:then}
     <main class="layout">
-        <div class="h-16 border-b border-white" />
-        <div class="h-full p-4"><slot /></div>
-        <div class="h-24 border-t border-white" />
+        <div class="header" />
+        <div class="content"><slot /></div>
+        <div class="player" />
     </main>
 {/await}
 
 <style lang="scss">
     main.layout {
         @apply w-screen h-screen flex flex-col;
+
+        div.header {
+            @apply h-16 border-b border-white;
+            min-height: 4rem;
+        }
+
+        div.content {
+            @apply h-full p-4 overflow-auto;
+        }
+
+        div.player {
+            @apply h-24 border-t border-white;
+            min-height: 6rem;
+        }
     }
 </style>
