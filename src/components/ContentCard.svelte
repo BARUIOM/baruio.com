48985b708f711eb4b0c783b59b6526a62b36896c
Vitor Mac <5874907+listanegra@users.noreply.github.com>
2022-05-09 20:57:11 -0300

    Content card component

---
+++ src/components/ContentCard.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    import { goto } from "@roxi/routify";
+
+    export let cover: string;
+    export let title: string;
+    export let meta: { title: string; href?: string }[] = [];
+    export let href: string;
+
+    const navigate = () => $goto(href);
+</script>
+
+<div class="card" on:click={navigate}>
+    <div class="cover" style="background-image: url({cover});" />
+
+    <div class="detail space-y-1">
+        <div class="truncate text-md font-bold">{title}</div>
+
+        {#each meta as item}
+            {#if item.href}
+                <a class="meta" href={item.href}>{item.title}</a>
+            {:else}
+                <div class="meta">{item.title}</div>
+            {/if}
+        {/each}
+    </div>
+</div>
+
+<style lang="scss">
+    div.card {
+        @apply h-full p-4 rounded bg-neutral-900 cursor-pointer space-y-4;
+
+        div.cover {
+            @apply bg-cover bg-center;
+            padding-bottom: 100%;
+        }
+
+        div.detail {
+            min-height: 64px;
+
+            .meta {
+                @apply text-sm text-neutral-400 truncate;
+            }
+        }
+    }
+</style>
